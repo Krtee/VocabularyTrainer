b3b27e1e31e4603cedbfc55bd30cc0f15483b2d7
Regina <rd027@hdm-stuttgart.de>
2020-05-17 16:53:59 +0200

    Minor changes

--- frontend/src/pages/VocabularyList.js
+++ frontend/src/pages/VocabularyList.js
@@ -26,54 +26,54 @@ import Navigation from "../components/Navigation";
                 <div id="vocabulary_list">
                     <h1>Vocabulary overview</h1>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_header">English</div>
-                        <div className="col-4 vocabulary_list_header">French</div>
-                        <div className="col-4 vocabulary_list_header">Progress</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_header">English</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_header">French</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_header">Progress</div>
                     </div>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_english">vocabulary</div>
-                        <div className="col-4 vocabulary_list_foreign">vocabulaire</div>
-                        <div className="col-4 vocabulary_list_progress">2</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_english">vocabulary</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_foreign">vocabulaire</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_progress">2</div>
                     </div>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_english">Monday</div>
-                        <div className="col-4 vocabulary_list_foreign">lundi</div>
-                        <div className="col-4 vocabulary_list_progress">1</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_english">Monday</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_foreign">lundi</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_progress">1</div>
                     </div>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_english">university</div>
-                        <div className="col-4 vocabulary_list_foreign">université</div>
-                        <div className="col-4 vocabulary_list_progress">3</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_english">university</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_foreign">université</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_progress">3</div>
                     </div>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_english">vocabulary</div>
-                        <div className="col-4 vocabulary_list_foreign">vocabulaire</div>
-                        <div className="col-4 vocabulary_list_progress">2</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_english">vocabulary</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_foreign">vocabulaire</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_progress">2</div>
                     </div>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_english">Monday</div>
-                        <div className="col-4 vocabulary_list_foreign">lundi</div>
-                        <div className="col-4 vocabulary_list_progress">1</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_english">Monday</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_foreign">lundi</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_progress">1</div>
                     </div>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_english">university</div>
-                        <div className="col-4 vocabulary_list_foreign">université</div>
-                        <div className="col-4 vocabulary_list_progress">3</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_english">university</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_foreign">université</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_progress">3</div>
                     </div>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_english">vocabulary</div>
-                        <div className="col-4 vocabulary_list_foreign">vocabulaire</div>
-                        <div className="col-4 vocabulary_list_progress">2</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_english">vocabulary</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_foreign">vocabulaire</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_progress">2</div>
                     </div>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_english">Monday</div>
-                        <div className="col-4 vocabulary_list_foreign">lundi</div>
-                        <div className="col-4 vocabulary_list_progress">1</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_english">Monday</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_foreign">lundi</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_progress">1</div>
                     </div>
                     <div className="row vocabulary_list_entry">
-                        <div className="col-4 vocabulary_list_english">university</div>
-                        <div className="col-4 vocabulary_list_foreign">université</div>
-                        <div className="col-4 vocabulary_list_progress">3</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_english">university</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_foreign">université</div>
+                        <div className="col-xl-1 col-lg-2 col-md-3 col-4 vocabulary_list_progress">3</div>
                     </div>
                 </div>
             </div>
