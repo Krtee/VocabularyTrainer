da40ab93bf953f0787e9598db4e634f5734fb202
Regina Dietrich <rd027@hdm-stuttgart.de>
2020-06-17 23:19:41 +0200

    SignUpButton.js: As function instead of class

--- frontend/src/components/SignUpButton.js
+++ frontend/src/components/SignUpButton.js
@@ -1,51 +1,42 @@
-import React from "react";
+import React, { useState } from "reactn";
 import LogInForms from "./LogInForms";
 
-class SignUpButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { noAccount: false, buttonText: "No account?" };
-    }
 
-    handleClick = () => {
-        const { emptyHelpFields } = this.props;
+const SignUpButton = (props) => {
+    const { createUser, login, userNameHelp, passwordHelp, passwordRepeatHelp, emptyHelpFields } = props;
+
+    const [noAccount, setNoAccount] = useState(false);
+    const [buttonText, setButtonText] = useState("No account?");
+
+
+    const handleClick = () => {
+        //const { emptyHelpFields } = this.props;
         emptyHelpFields();
-        if (this.state.noAccount === false) {
-            this.setState({
-                noAccount: true,
-                buttonText: "Already got an account?"
-            });
+        if (noAccount === false) {
+            setNoAccount(true);
+            setButtonText("Already got an account?");
         } else {
-            this.setState({
-                noAccount: false,
-                buttonText: "No account?"
-            });
+            setNoAccount(false);
+            setButtonText("No account?");
             emptyHelpFields();
         }
 
     };
 
-    render() {
-        const { createUser, login } = this.props;
-        const { userNameHelp } = this.props;
-        const { passwordHelp } = this.props;
-        const { passwordRepeatHelp } = this.props;
+    return (
+        <div>
+            <LogInForms noAccount={noAccount} createUser={createUser} login={login} userNameHelp={userNameHelp} passwordHelp={passwordHelp} passwordRepeatHelp={passwordRepeatHelp}/>
+            <button
+                type="button"
+                className="btn btn-primary marginleft margin_top grey_button"
+                onClick={handleClick}
+            >
+                {buttonText}
+            </button>
+        </div>
+    );
 
-        return (
-            <div>
-                <LogInForms noAccount={this.state.noAccount} createUser={createUser} login={login} userNameHelp={userNameHelp} passwordHelp={passwordHelp} passwordRepeatHelp={passwordRepeatHelp}/>
-                <button
-                    type="button"
-                    className="btn btn-primary marginleft margin_top grey_button"
-                    onClick={this.handleClick}
-                >
-                    {this.state.buttonText}
-                </button>
-            </div>
-        );
-    }
-}
+};
 
 export default SignUpButton;
-
 
