af21570fd1ec2dc7fe0d5a2f35944f42499c5062
rg041 <rg041@hdm-stuttgart.de>
2020-06-17 15:34:46 +0200

    versioning

--- bumpVersion.js
+++ bumpVersion.js
@@ -5,63 +5,23 @@ console.log("Updating git commit ....: ");
 const assert = require("assert");
 const fs = require("fs");
 const path = require("path");
-const { spawn } = require("child_process");
+
 
 const pathVersionFile = path.join(__dirname, "./frontend/src/lib/version.json");
-console.log("pathVersionFile", pathVersionFile);
-
 let versionNumber = version;
 const tmp = versionNumber.split(".");
 tmp[2] = parseInt(tmp[2]) + 1;
 
 versionNumber = `${tmp[0]}.${tmp[1]}.${tmp[2]}`;
-console.log("versionNumber", versionNumber);
 
 fs.readFile(pathVersionFile, (err, buffer) => {
   assert.equal(err, null);
 
   const data = buffer.toString("utf-8");
-  console.log("data", data);
   const updatedData = data.replace(/"version"\s*:.*/gim, `"version": "${versionNumber}"`);
-  console.log("updatedData", updatedData);
 
   fs.writeFile(pathVersionFile, updatedData, (err) => {
     assert.equal(err, null);
     console.log(`Updating git commit done: ${versionNumber}`);
   });
 });
-
-// // Get the current commit date
-// let gitCommitDate = ""
-// const execGitDate = spawn("git", [
-//   "show",
-//   "--date=local",
-//   "--no-patch",
-//   "--no-notes",
-//   "--pretty='%cd'",
-//   "HEAD"
-// ])
-// execGitDate.stdout.on("data", chunk => {
-//   try {
-//     gitCommitDate = chunk.toString("utf-8")
-//     gitCommitDate.substring(1, 16)
-
-//     // Update git date
-//     fs.readFile(pathVersionFile, (err, buffer) => {
-//       assert.equal(err, null)
-
-//       const data = buffer.toString("utf-8")
-//       const updatedData = data.replace(
-//         /MET_SERVICES_SELF_LASTCHANGEDATE=.*/gm,
-//         `MET_SERVICES_SELF_LASTCHANGEDATE=${gitCommitDate}`
-//       )
-
-//       fs.writeFile(pathVersionFile, updatedData, err => {
-//         assert.equal(err, null)
-//         console.log(`Updating git commit date done: 👍🆗 ${gitCommitDate}`)
-//       })
-//     })
-//   } catch (error) {
-//     console.error(error)
-//   }
-// })
